e37bf4978eae11deb74f34ee7e57da244c3fd90c
Mohamed Belal <muhammed1belal@gmail.com>
2023-07-25 18:52:46 +0300

    Done

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-   * prints_array - prints n elements of an array of integers
+   * print_array - prints n elements of an array of integers
    *
    * @n: elements parameter input
    * @a: string parameter input
@@ -16,9 +16,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
-			printf("@d ", a[i]);
+			printf("%d ", a[i]);
 		else
-			printf("@d ", a[i]);
+			printf("%d ", a[i]);
 	}
 	printf("\n");
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * *_stropy - copies the string pointed to by src
+  * *_strcpy - copies the string pointed to by src
   * @dest: char type string
   * @src: char typr string
   * Description:  copy the string pointed to by pointer `src` to
@@ -9,7 +9,7 @@
   * Return: pointer to `dest`
   */
 
-char *_stropy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
 
@@ -18,5 +18,5 @@ char *_stropy(char *dest, char *src)
 		dest[i] = src[i];
 	} while (src[i] != '\0');
 
-	return (dest);`
+	return (dest);
 }
