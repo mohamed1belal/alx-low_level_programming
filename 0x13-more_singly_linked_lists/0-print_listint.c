373d9c96428c8e215094d24e525af95b1c0a917f
Mohamed Belal <muhammed1belal@gmail.com>
2023-08-29 11:55:54 +0300

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
+	const listint_t *current = h;
 
-	while (h)
+	while (current)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", current->n);
+		current = current->next;
 		count++;
 	}
 	return (count);
