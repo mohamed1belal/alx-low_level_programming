06c45c8b65312eb985aef11ad0c787a4d8549452
Mohamed Belal <muhammed1belal@gmail.com>
2023-11-27 20:10:12 +0200

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,6 +20,31 @@ char *create_buffer(char *file)
 	buff = malloc(sizeof(char) * 1024);
 
 	if (buff == NULL)
+=======
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to file %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define READ_BUF_SIZE 1024
+
+/**
+  * main - program
+  * @ac: arg count
+  * @av: arg vector
+  *
+  * Return: 1 on success 0 on failure
+  */
+
+int main(int ac, char **av)
+{
+	ssize_t bytes_read, bytes_written;
+
+	int copy_file(const char *src_filename, const char *dest_filename);
+
+	if (ac != 3)
+>>>>>>> eacde93 (test)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
@@ -41,6 +67,7 @@ void close_file(int fd)
 
 	if (m == -1)
 	{
+<<<<<<< HEAD
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
@@ -97,4 +124,63 @@ int main(int argc, char *argv[])
 	close_file(to);
 
 	return (0);
+=======
+		return (1);
+	}
+	else
+	{
+		return (98);
+	}
+}
+
+int copy_file(const char *src_filename, const char *dest_filename)
+{
+	int from_fd = open(src_filename, O_RDONLY);
+
+	if (from_fd == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOREAD, src_filename);
+		return (0);
+	}
+
+	int to_fd = open(dest_filename, O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+
+	if (to_fd == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOWRITE, dest_filename);
+		close(from_fd);
+		return (0);
+	}
+
+	char buffer[READ_BUF_SIZE];
+
+	while ((bytes_read = read(from_fd, buffer, READ_BUF_SIZE)) > 0)
+	{
+		bytes_written = write(to_fd, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, ERR_NOWRITE, dest_filename);
+			close(from_fd);
+			close(to_fd);
+			return (0);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOREAD, src_filename);
+		close(from_fd);
+		close(to_fd);
+		return (0);
+	}
+
+	if (close(from_fd) == -1 || close(to_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOCLOSE,
+				(close(from_fd) == -1) ? from_fd : to_fd);
+		return (0);
+	}
+
+	return (1);
+>>>>>>> eacde93 (test)
 }
